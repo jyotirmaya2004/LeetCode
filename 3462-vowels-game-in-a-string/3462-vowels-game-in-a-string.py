8a30f05314b14b3683742776bff0017d3caba577
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2025-09-15 07:52:18 +0530

    Time: 4 ms (92.51%), Space: 18 MB (77.13%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -0,0 +1,6 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        count = s.count('a') + s.count('e') + s.count('i') + s.count('o') + s.count('u')
+        if count == 0: return False
+        else: return True
+        
