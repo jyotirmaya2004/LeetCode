424ae584a72559a59f6e2c3e747c7def0aa89e2b
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2025-10-31 00:10:32 +0530

    Time: 11 ms (92.43%), Space: 28.2 MB (63.65%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.py
@@ -1,26 +1,25 @@
+from collections import deque
+
 class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
-        res = 0
-        q = deque(initialBoxes)
-        visit = set()
-        while q:
-            for i in range(len(q)):
-                curBox = q.popleft()
-                if curBox in visit:
-                    continue
-                visit.add(curBox)
-                
-                if status[curBox]:
-                    res += candies[curBox]
-                    # Update status and/or reset visit and q if already visited
-                    for k in keys[curBox]:
-                        if k in visit and status[k] == 0:
-                            status[k] = 1
-                            q.append(k)
-                            visit.remove(k)
-                        else:
-                            status[k] = 1
-                    # add boxes found in current box to q
-                    for box in containedBoxes[curBox]:
-                        q.append(box)
-        return res
+
+        boxes = deque(initialBoxes)
+        total_candies = 0
+        while boxes:
+            box = boxes.popleft()
+
+            if status[box] != 1:
+                status[box] = -1
+                continue
+
+            total_candies += candies[box]
+
+            for new_box in containedBoxes[box]:
+                boxes.append(new_box)
+
+            for key in keys[box]:
+                if status[key] == -1:
+                    boxes.append(key)
+                status[key] = 1
+            
+        return total_candies
