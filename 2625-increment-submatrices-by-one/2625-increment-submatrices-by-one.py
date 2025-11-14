5f872f1c2b23a6d4dc58c48e1dc31727f7523cd8
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2025-11-14 06:30:36 +0530

    Time: 283 ms (39.9%), Space: 41.6 MB (14.55%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -1,12 +1,30 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        ans = [[0] * n for _ in range(n)]
-        for r1, c1, r2, c2 in queries:
-            for r in range(r1, r2 + 1):
-                ans[r][c1] += 1
-                if c2 + 1 < n: ans[r][c2 + 1] -= 1
-        for r in range(n):
-            for c in range(1, n):
-                ans[r][c] += ans[r][c - 1]
-        return ans
+        grid = [[0] * (n+1) for _ in range(n+1)]
+
+        for start_x, start_y, end_x, end_y in queries:
+            grid[start_x][start_y] += 1
+            if end_y + 1 < n:
+                grid[start_x][end_y + 1] -= 1
+            
+            if end_x + 1 < n:
+                grid[end_x + 1][start_y] -= 1
+
+            if end_x + 1 < n and end_y + 1 < n:
+                grid[end_x+1][end_y+1] += 1
         
+        for row in range(n):
+            for col in range(n):
+                if row > 0:
+                    grid[row][col] += grid[row-1][col]
+                
+                if col > 0:
+                    grid[row][col] += grid[row][col-1]
+                
+                if row > 0 and col > 0:
+                    grid[row][col] -= grid[row-1][col-1]
+                    
+        
+        print(grid)
+        result = [row[:n] for row in grid[:n]]
+        return result
