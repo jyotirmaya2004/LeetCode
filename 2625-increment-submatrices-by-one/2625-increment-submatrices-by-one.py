b6a99259f43d25f3cc356060e4aadcb335a045fc
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2025-11-14 06:31:06 +0530

    Time: 107 ms (98.59%), Space: 38 MB (77%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -1,30 +1,22 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        grid = [[0] * (n+1) for _ in range(n+1)]
-
-        for start_x, start_y, end_x, end_y in queries:
-            grid[start_x][start_y] += 1
-            if end_y + 1 < n:
-                grid[start_x][end_y + 1] -= 1
-            
-            if end_x + 1 < n:
-                grid[end_x + 1][start_y] -= 1
-
-            if end_x + 1 < n and end_y + 1 < n:
-                grid[end_x+1][end_y+1] += 1
+        res = [[0] * n for _ in range(n)]
+        for r1, c1, r2, c2 in queries:
+            r2 += 1; c2 += 1;
+            res[r1][c1] += 1
+            if c2 < n:
+                res[r1][c2] -= 1
+            if r2 < n:
+                res[r2][c1] -= 1
+                if c2 < n:
+                    res[r2][c2] += 1
         
-        for row in range(n):
-            for col in range(n):
-                if row > 0:
-                    grid[row][col] += grid[row-1][col]
-                
-                if col > 0:
-                    grid[row][col] += grid[row][col-1]
-                
-                if row > 0 and col > 0:
-                    grid[row][col] -= grid[row-1][col-1]
-                    
-        
-        print(grid)
-        result = [row[:n] for row in grid[:n]]
-        return result
+        delta = [0] * n
+        for i, row in enumerate(res):
+            acc = 0
+            for j, x in enumerate(row):
+                delta[j] += x
+                acc += delta[j]
+                res[i][j] = acc 
+
+        return res
