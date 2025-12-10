2c494263b15a612d273b671ffd1dbcb5e811a69e
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2025-12-10 12:18:04 +0530

    Time: 7 ms (98.89%), Space: 43.7 MB (8.33%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -1,3 +1,17 @@
+MOD = 10**9 + 7
+facs = {}
+facs[1] = 1
+facs[2] = 2
+for i in range(2,(10**5)+1):
+    facs[i] = (facs[i-1] * i) % MOD
+
 class Solution:
-    def countPermutations(self, x: List[int]) -> int:
-        return 0 if x[0]>=min(x[1:]) else factorial(len(x)-1)%(10**9+7)
+    def countPermutations(self, complexity: List[int]) -> int:
+
+        lowest = complexity[0]
+        for c in complexity[1:]:
+            if c <= lowest:
+                return 0 
+        return facs[len(complexity)-1]
+        
+
