6d531bc6e4c56bfc0e0a3d6e4324bb98ea63d597
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2026-01-04 10:19:23 +0530

    Time: 0 ms (100%), Space: 17.4 MB (56.85%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,8 +1,26 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = "".join(str(x) for x in digits)
-        num=int(s)
-        num+=1
-        s2=str(num)
-        return [int(x) for x in (s2)]
+        n = len(digits)
+        sum_val = 0
+        carry = 0
         
+        # Traverse from the last digit to the first
+        for i in range(n - 1, -1, -1):
+            # Add 1 only if it is the last digit, otherwise just add the carry
+            adder = 1 if i == n - 1 else 0
+            sum_val = digits[i] + adder + carry
+            
+            # If no carry is generated, update and return
+            if sum_val <= 9:
+                digits[i] = sum_val
+                return digits
+            
+            # If carry is generated
+            digits[i] = sum_val % 10
+            carry = sum_val // 10
+            
+        # If the loop completes and carry is still non-zero
+        if carry != 0:
+            digits.insert(0, carry)
+            
+        return digits
