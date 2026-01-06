7c601b443030ad791c9fbff3c7a7cc60900b04c0
Jyotirmaya Behera <jyotirmaybehera2020@gmail.com>
2026-01-06 17:18:51 +0530

    Time: 107 ms (70.5%), Space: 37.8 MB (64.79%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -1,20 +1,25 @@
 class Solution:
+    def dfs(self, node, values, visited, tree, k):
+        visited[node] = True
+        total = values[node]
+
+        for connected_node in tree[node]:
+            if not visited[connected_node]:
+                total += self.dfs(connected_node, values, visited, tree, k)
+
+        if total % k == 0:
+            self.ans += 1
+            return 0
+        return total
+
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        graph = [[] for _ in range(n)]
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-
         self.ans = 0
-        def dfs(root, parent):
-            s = values[root]
-            for child in graph[root]:
-                if child != parent:
-                    s += dfs(child, root)
-            if s%k == 0:
-                self.ans += 1
-                return 0
-            return s
+        visited = [False] * n
+        tree = defaultdict(list)
 
-        dfs(0, -1)
+        for u, v in edges:
+            tree[u].append(v)
+            tree[v].append(u)
+        
+        self.dfs(0, values, visited, tree, k)
         return self.ans
